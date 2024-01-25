58fe30cc752a95555195c53315dee9c97d44900c
Isaac QS <inqs.909@gmail.com>
2024-01-24 17:17:35 -0800

    Possible Fix for missing learnr dependency

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,4 +1,4 @@
-#' @importFrom learnr question question_text quiz
+#' @import learnr
 #' @importFrom ggplot2 ggplot geom_point aes geom_histogram
 #' @importFrom learnrhash encoder_logic encoder_ui
 #' @importFrom gradethis grade_result pass_if grade_this_code
