f6ea1620a84dfd5a9112987bc3eccba1e23f4ded
Isaac QS <inqs.909@gmail.com>
2024-07-08 11:39:21 -0700

    Removed learnr tutorials

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,8 +1,3 @@
-#' @import learnr
-#' @importFrom ggplot2 ggplot geom_point aes geom_histogram
-#' @importFrom learnrhash encoder_logic encoder_ui
-#' @importFrom gradethis grade_result pass_if grade_this_code
-#' @importFrom ggcheck uses_geoms
-#' @import rsconnect
+#' @import ggplot2
 #'
 NULL
