658261481055bb3075ad088efbca61eba3e3f49e
Isaac QS <inqs.909@gmail.com>
2024-10-10 15:44:41 -0700

    Fixed props to allow shuffle function.

--- R/props.R
+++ R/props.R
@@ -2,22 +2,22 @@
 #'
 #' Compute the proportions of observing a value from a 2 by 2 continguency table
 #'
-#' @param df Name of the data frame
 #' @param x grouping variable
 #' @param y outcome of interest
 #' @param yval category of interest from outcome of interest
-#' @param diff Obtaine the difference in proportions. Default is TRUE.
+#' @param diff Obtaine the difference in proportions. Default is FALSE.
 #'
 #' @export
 #'
-props <- function(df, x, y, yval, diff = TRUE){
+props <- function(x, y, yval, diff = FALSE){
   n <- p <- NULL
+  df <- tibble::tibble(x = as.character(x), y = as.character(y))
   suppressMessages(
-    res <- df |> dplyr::group_by({{x}}, {{y}}) |>
+    res <- df |> dplyr::group_by(x, y) |>
       dplyr::summarise(n = n()) |>
       dplyr::mutate(p = n / sum(n)) |>
       dplyr::ungroup() |>
-      dplyr::filter({{y}} == yval) |>
+      dplyr::filter(y == as.character(yval)) |>
       dplyr::select(p) |>
       unlist()
   )
